d0374dc52daa39a08f99a8af9895cf9d69447bcd
faizdaud <ahmadfaizdaud007@gmail.com>
2021-10-01 07:40:46 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,31 @@
+var express = require('express'); // call express
+var app = express();
+var bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+
+
+var port = process.env.PORT || 8080;
+
+
+var router = express.Router();
+
+router.get('/', function(req, res) {
+    res.json({ message: 'hooray! welcome to our api!' });
+    });
+
+router.get('/whoami', function(req, res){
+    var ipAdress = req.headers['x-forwarded-for'];
+    var operatingSystem = req.headers['user-agent'];
+    var langauge = req.headers["accept-language"];  
+    res.json({
+        ipAddress: ipAdress,
+        operatingSystem: operatingSystem,
+        language: langauge
+    })
+})
+
+app.use('/api', router);
+app.listen(port);
+console.log('Magic happens on port ' + port);
